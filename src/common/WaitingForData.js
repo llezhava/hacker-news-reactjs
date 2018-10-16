90bf37fd23afedc4081eea12d3d4dbc8e8ad12e3
Lasha Lezhava <lashalezhava@gmail.com>
2018-10-16 23:24:43 +0400

    Only items showed in the current page of a Story component is loaded from the api. WaitingForData render props is used for each story item, so I can add spinner to each story in future

--- src/common/WaitingForData.js
+++ src/common/WaitingForData.js
@@ -3,13 +3,10 @@ import React, {Component} from "react"
 function IsFetching(props) {
     let {isFetching} = props
 
-    console.log(isFetching)
-
-    console.log("IsFetching", isFetching)
     if(isFetching) {
         return "Loading..."
     } else {
-        return <div>{props.children}</div>
+        return props.children
     }
 }
 
--- src/story/Stories.js
+++ src/story/Stories.js
@@ -1,121 +1,15 @@
 import React, { Component } from "react";
-import Page from "./Page";
 import Pagination from "./Pagination";
-import { getStory, getItem , getStories} from "../services/hackernewsapi";
-import Data from "./Data"
-import WaitingForData from "../common/WaitingForData"
-
-const initialState = {
-  currentDisplay: 0,
-  stories: [],
-  pageNumber: 0,
-  path: undefined
-};
-
-const style= {
-  height: "100%",
-  width: "100%"
-}
-
-class Storiesx extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      stories: [],
-      pageNumber: props.match.params.page || 0,
-      path: this.getPath(props.match.path),
-      hasError: false,
-      isFetching: true
-    };
-  }
-
-  getPath(path) {
-    let regex = /\/:.*/gi;
-    return path.replace(regex, "");
-  }
-
-  componentDidMount() {
-   this.updateStoryItems(this.props.category);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.category !== this.props.category) {
-      this.setState({ stories: [] });
-      this.updateStoryItems(this.props.category);
-    }
-
-    if (prevProps.match.params.page !== this.props.match.params.page) {
-      this.setState({
-        stories: [],
-        pageNumber: this.props.match.params.page
-      });
-      this.updateStoryItems(this.props.category);
-    }
-
-    if (prevProps.match.path !== this.props.match.path) {
-      this.setState({ stories: [], path: this.getPath(this.props.match.path) });
-      this.updateStoryItems(this.props.category);
-    }
-
-    if (prevProps.match.url !== this.props.match.url) {
-      this.setState({ stories: [], path: this.getPath(this.props.match.path) });
-      this.updateStoryItems();
-    }
-  }
-
-  componentDidCatch(error, info) {
-    this.setState({ hasError: true });
-    // You can also log the error to an error reporting service
-  }
-
-  updateStoryItems() {
-    this.setState({isFetching: true}, this.getNewStories)
-    
-  }
-
-  getNewStories() {
-    let category = this.props.category;
-
-    getStories(category).then(rawStories => {
-      let stories = this.getItems(rawStories, this.state.pageNumber).map(
-        story => {
-          return { story: story.item.val(), index: story.index };
-        }
-      );
-      this.setState({ stories, isFetching: false });
-    });
-  }
-
-  getItems(stories, pageNumber, itemsPerPage = 50) {
-    let currentStart = itemsPerPage * pageNumber;
-    let currentItems = stories
-      .slice(currentStart, currentStart + itemsPerPage)
-      .map((story, index) => {
-        return { item: story, index: index + currentStart };
-      });
-    return currentItems;
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <div className="error">Error occured!</div>;
-    }
-
-    return (
-      <div className="container">
-      <WaitingForData isFetching={this.state.isFetching}>
-        <Page items={this.state.stories} style={style} />
-        </WaitingForData>
-        <Pagination pageNumber={this.state.pageNumber} path={this.state.path} />
-      </div>
-    );
-  }
-}
+import { getStory } from "../services/hackernewsapi";
+import SingleItem from "./SingleItem";
 
 class Stories extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      /*
+       [{item: 18231097, index: 0}]
+      */
       itemsList: [],
       pageNumber: props.match.params.page || 0,
       path: this.getPath(props.match.path),
@@ -140,19 +34,25 @@ class Stories extends Component {
 
     if (prevProps.match.params.page !== this.props.match.params.page) {
       this.setState({
-        stories: [],
+        itemsList: [],
         pageNumber: this.props.match.params.page
       });
       this.updateStoryItems(this.props.category);
     }
 
     if (prevProps.match.path !== this.props.match.path) {
-      this.setState({ stories: [], path: this.getPath(this.props.match.path) });
+      this.setState({
+        itemsList: [],
+        path: this.getPath(this.props.match.path)
+      });
       this.updateStoryItems(this.props.category);
     }
 
     if (prevProps.match.url !== this.props.match.url) {
-      this.setState({ stories: [], path: this.getPath(this.props.match.path) });
+      this.setState({
+        itemsList: [],
+        path: this.getPath(this.props.match.path)
+      });
       this.updateStoryItems();
     }
   }
@@ -163,12 +63,13 @@ class Stories extends Component {
   }
 
   updateStoryItems() {
-    let {category} = this.props;
+    let { category } = this.props;
 
     getStory(category).then(idList => {
-      let items = this.getItems(idList, this.state.pageNumber)
-      this.setState({idList: items})
-    })
+      console.log(idList);
+      let itemsList = this.getItems(idList, this.state.pageNumber);
+      this.setState({ itemsList });
+    });
   }
 
   getItems(stories, pageNumber, itemsPerPage = 50) {
@@ -176,7 +77,7 @@ class Stories extends Component {
     let currentItems = stories
       .slice(currentStart, currentStart + itemsPerPage)
       .map((story, index) => {
-        return { item: story, index: index + currentStart };
+        return { id: story, index: index + currentStart };
       });
     return currentItems;
   }
@@ -187,13 +88,15 @@ class Stories extends Component {
     }
     return (
       <div className="container">
-        <Page items={this.state.itemsList} />
+        {this.state.itemsList.map((id, index) => (
+          <div className="storyItem">
+          <SingleItem key={index} id={id.id} />
+          </div>
+        ))}
         <Pagination pageNumber={this.state.pageNumber} path={this.state.path} />
       </div>
     );
   }
 }
-
-
 
 export default Stories;
