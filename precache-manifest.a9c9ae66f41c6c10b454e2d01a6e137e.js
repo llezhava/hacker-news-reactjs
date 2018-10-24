9200ca17c4f5629e8a65abef985b21ed62c12606
Lasha Lezhava <lashalezhava@gmail.com>
2018-10-24 20:08:23 +0400

    Updates

---
+++ precache-manifest.a9c9ae66f41c6c10b454e2d01a6e137e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f17281bc5e2e52919b4",
+    "url": "/static/js/main.7f17281b.chunk.js"
+  },
+  {
+    "revision": "3ec26eba51c4671c82cb",
+    "url": "/static/js/1.3ec26eba.chunk.js"
+  },
+  {
+    "revision": "7f17281bc5e2e52919b4",
+    "url": "/static/css/main.9dd7373b.chunk.css"
+  },
+  {
+    "revision": "ac01ce5bc9d5a9c49d66a6a6c0419f01",
+    "url": "/index.html"
+  }
+];
