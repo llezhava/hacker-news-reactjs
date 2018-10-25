3c52e0b59abd99e6e2a19f5347ea774d1b3ead74
Lasha Lezhava <lashalezhava@gmail.com>
2018-10-25 13:23:22 +0400

    Updates

--- precache-manifest.d6b19919f478dfb5f1c1bfc33375d3f2.js
+++ precache-manifest.d6b19919f478dfb5f1c1bfc33375d3f2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/hacker-news-reactjs/static/js/runtime~main.9ad19288.js"
   },
   {
-    "revision": "7cc29d34a5d79415608d",
-    "url": "/hacker-news-reactjs/static/js/main.7cc29d34.chunk.js"
+    "revision": "b54e16b3f3375eda4caf",
+    "url": "/hacker-news-reactjs/static/js/main.b54e16b3.chunk.js"
   },
   {
     "revision": "fe0749678c22088ec2ef",
     "url": "/hacker-news-reactjs/static/js/1.fe074967.chunk.js"
   },
   {
-    "revision": "7cc29d34a5d79415608d",
+    "revision": "b54e16b3f3375eda4caf",
     "url": "/hacker-news-reactjs/static/css/main.9dd7373b.chunk.css"
   },
   {
-    "revision": "132ceaf6693ca0e9a87574dbfa2b9e63",
+    "revision": "4d8b069f4458cc2622e08469f727f4cd",
     "url": "/hacker-news-reactjs/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/hacker-news-reactjs/precache-manifest.512642469a753101450fc4c6a1783d7a.js"
+  "/hacker-news-reactjs/precache-manifest.d6b19919f478dfb5f1c1bfc33375d3f2.js"
 );
 
 workbox.clientsClaim();
